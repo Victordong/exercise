97aef6f9601648ffce4626476c5b9483a68b5347
zhandong <1468767640@qq.com>
2020-01-06 19:18:15 +0800

    first day

---
+++ str.cpp
@@ -0,0 +1,18 @@
+//
+// Created by zhandong on 2020-01-06.
+//
+
+#include <string>
+using namespace std;
+
+class Rotation {
+public:
+    bool chkRotation(string A, int lena, string B, int lenb) {
+        // write code here
+        if(lena!= lenb) {
+            return false;
+        }
+        string new_string = B+B;
+        return new_string.find(A)!= string::npos;
+    }
+};
