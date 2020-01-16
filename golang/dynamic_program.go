45ef972eeeb8bd16bb84e15bfe67d4b535ade386
zhandong <1468767640@qq.com>
2020-01-16 15:42:52 +0800

    begin golang

--- golang/dynamic_program.go
+++ golang/dynamic_program.go
@@ -87,14 +87,6 @@ func wordBreak(s string, wordDict []string) bool {
 	return false
 }
 
-func min(first int, second int) int {
-	if first < second {
-		return first
-	} else {
-		return second
-	}
-}
-
 func minimumTotal(triangle [][]int) int {
 	for i := 1; i < len(triangle); i++ {
 		triangle[i][0] += triangle[i-1][0]
@@ -110,4 +102,91 @@ func minimumTotal(triangle [][]int) int {
 		}
 	}
 	return min
+}
+
+func min(first int, second int) int {
+	if first < second {
+		return first
+	} else {
+		return second
+	}
+}
+
+func max(first int, second int) int {
+	if first < second {
+		return second
+	} else {
+		return first
+	}
+}
+
+func maxProduct(nums []int) int {
+	var imax = 1
+	var imin = 1
+	var max_num = 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] < 0 {
+			temp := imax
+			imax = imin
+			imin = temp
+		}
+		imax = max(imax*nums[i], nums[i])
+		imin = min(imin*nums[i], nums[i])
+		max_num = max(max_num, imax)
+	}
+	return imax
+}
+
+func maxProfit1(prices []int) int {
+	min := prices[0]
+	max := ^min
+	for i := 1; i < len(prices); i++ {
+		if prices[i]-min > max {
+			max = prices[i] - min
+		}
+		if prices[i] < min {
+			min = prices[i]
+		}
+	}
+	if max < 0 {
+		return 0
+	} else {
+		return max
+	}
+}
+
+func maxProfit2(prices []int) int {
+	n := len(prices)
+	var b [100000]int
+	b[0] = 0
+	for i := 1; i < n; i++ {
+		max := 0
+		for j := 0; j < i; j++ {
+			var val int
+			if prices[i] > prices[j] {
+				val = 0
+			} else {
+				val = prices[j] - prices[i]
+			}
+			if max < b[j]+val {
+				max = b[j] + val
+			}
+		}
+		b[i] = max
+	}
+	return b[n-1]
+}
+
+func maxProfit(prices []int) int {
+	n := len(prices)
+	if n == 0 {
+		return 0
+	}
+	max := 0
+	for i := 0; i < n-1; i++ {
+		if prices[i] < prices[i+1] {
+			max += prices[i+1] - prices[i]
+		}
+	}
+	return max
 }
