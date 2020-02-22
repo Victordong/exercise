ba54ab3fd16625f143b03caf97a68c34a85f7b1a
zhandong <1468767640@qq.com>
2020-02-22 16:24:47 +0800

    some test about chan

--- golang/main.go
+++ golang/main.go
@@ -1,12 +1,60 @@
 package golang
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func FirstWrite(writeChan <-chan bool, readChan chan<- bool, endChan <-chan bool) {
+	for {
+		select {
+		case <-endChan:
+			fmt.Println("close")
+			return
+		case <-writeChan:
+			fmt.Println("write")
+			time.Sleep(time.Second)
+			readChan <- true
+		}
+	}
+
+}
+
+func SecondRead(writeChan chan<- bool, readChan <-chan bool, endChan <-chan bool) {
+	for {
+		select {
+		case <-endChan:
+			fmt.Println("close")
+			return
+		case <-readChan:
+			fmt.Println("read")
+			time.Sleep(time.Second)
+			writeChan <- true
+		}
+	}
+
+}
 
 func Main() {
-	m := make(map[interface{}]int)
-	m['a'] = 10
-	m[10] = 11
-	m["100"] = 12
-	m[97] = 10000
-	fmt.Println(m[97], m['a'], m["100"])
+	//var wg sync.WaitGroup
+	wChan, rChan, eChan := make(chan bool, 1), make(chan bool, 1), make(chan bool)
+	finalChan := make(chan bool, 2)
+	go func(writeChan chan bool, readChan chan bool, endChan chan bool) {
+		defer func() {
+			finalChan <- true
+		}()
+		FirstWrite(writeChan, readChan, endChan)
+	}(wChan, rChan, eChan)
+	go func(writeChan chan bool, readChan chan bool, endChan chan bool) {
+		defer func() {
+			finalChan <- true
+		}()
+		SecondRead(writeChan, readChan, endChan)
+	}(wChan, rChan, eChan)
+	wChan <- true
+	time.Sleep(10 * time.Second)
+	close(eChan)
+	for i := 0; i < 2; i++ {
+		<-finalChan
+	}
 }
