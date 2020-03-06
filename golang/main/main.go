5e25beb3168a90d93b2dcdaac00c53abebac6d80
zhandong <1468767640@qq.com>
2020-03-06 22:30:55 +0800

    begin balance tree

--- golang/main/main.go
+++ golang/main/main.go
@@ -1,7 +1,40 @@
 package main
 
-import al "github.com/Victordong/exercise"
+import (
+	"fmt"
+	"reflect"
+)
+
+type Wallet struct {
+	Owner string
+	Num   float64
+}
 
 func main() {
-	al.Main()
+	w := &Wallet{
+		Owner: "nicai",
+		Num:   11,
+	}
+	wTypeOf := reflect.TypeOf(w)
+	wValueOf := reflect.ValueOf(w)
+	if wTypeOf.Kind() == reflect.Ptr {
+		wTypeOf = wTypeOf.Elem()
+	}
+	if wValueOf.Kind() == reflect.Ptr {
+		wValueOf = wValueOf.Elem()
+	}
+	fmt.Println(wValueOf.FieldByName("Owner").Type())
+	if wValueOf.CanAddr() {
+		fmt.Println(wValueOf.Addr())
+		fmt.Println("can")
+	} else {
+		fmt.Println("can not")
+		if wValueOf.FieldByName("Owner").CanSet() {
+			//newValue := reflect.New(wValueOf.Elem().FieldByName("Owner").Type())
+			//wValueOf.Elem().FieldByName("Owner").Set()
+			fmt.Println("can set")
+		} else {
+			fmt.Println("can not set")
+		}
+	}
 }
