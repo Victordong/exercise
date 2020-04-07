378061b2bbe526a18fc2c4a41a4e3665cd527f56
zhandong <1468767640@qq.com>
2020-04-07 20:38:19 +0800

    exercise c++ everyday

--- c++/some_test.cpp
+++ c++/some_test.cpp
@@ -12,6 +12,43 @@ void timeout() {
     g_loop->quit();
 }
 
+using Entry = std::pair<std::string, int>;
+using EntryList = std::vector<Entry>;
+using Map = std::map<string, EntryList>;
+using MapPtr = std::shared_ptr<Map>;
+
+Maptr CustomData::getData() {
+    std::lock_guard<std::mutex>(lock_);
+    return data_;
+}
+
+int CustomData::query(const std::string& customer, const std::string& stock) {
+    MapPtr data = getData();
+    Map::const_iterator entries = data->find(customer);
+    if (entries != data->end()) {
+        return findEntry(entries->second, stock);
+    } else {
+        return -1;
+    }
+}
+
+void CustomData::update(const std::string& customer, const EntryList& entries) {
+    std::lock_guard<std::mutex>(lock_);
+    if (!data_.unique()) {
+        MapPtr newData(new Map(*data_));
+    }
+    (*data_)[customer] = entries;
+}
+
+MapPtr parseData(const std::string& message);
+void CustomData::update(const std::string& message) {
+    MapPtr newData = parseData(message);
+    if (newData) {
+        std::lock_guard<std::mutex>(lock_);
+        data_.swap(newData);
+    }
+}
+
 int main(int argc, char const* argv[]) {
     archer::Eventloop loop;
 
